79088b44288c35f88660acc618c90eb003b7175e
PappKriszti99 <157489916+PappKriszti99@users.noreply.github.com>
2024-01-23 16:14:13 +0100

    Add files via upload

---
+++ páros vagy páratlan/main.py
@@ -0,0 +1,13 @@
+def main() -> None:
+    pass  # Kezd a kódolást itt!
+
+
+print("páros páratlan meghatározása!")
+x: int = int(input("x="))
+if x % 2 == 0:
+    print("A szám páros!")
+else:
+    print("A szám páratlan")
+
+if __name__ == "__main__":
+    main()
