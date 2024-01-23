38cca2976b397d2c5f90dc92a6bd3b2b4ac96b9a
PappKriszti99 <157489916+PappKriszti99@users.noreply.github.com>
2024-01-23 15:54:56 +0100

    Add files via upload

---
+++ 2022.10.26_Osztalyok/Diak.py
@@ -0,0 +1,9 @@
+class Diak:
+    def __init__(self, nev, kor, neme, magassag):
+        self.nev = nev
+        self.kor = kor
+        self.neme = neme
+        self.magassag = magassag
+
+    def Koszontes(self):
+        print(f"Szevasz {self.nev}")
---
+++ 2022.10.26_Osztalyok/main.py
@@ -0,0 +1,6 @@
+from Diak import *
+from Diak import Diak
+
+d1 = Diak("Ress Dominik", 16, "férfi", 180)
+print(d1.nev)
+d1.Koszontes()
